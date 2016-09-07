55c3a14e3445b7aa882495ee0eefeac082403554
Tanner Wells <tannerjameswells@gmail.com>
2016-09-07 16:09:23 -0400

    adds google maps to select location on upload page

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -43,7 +43,7 @@ $(document).ready(function(){
 	            $('#preview').attr('src', e.target.result);
 	            $('#preview').css('display', 'block');
 	            $('#photo-info').show();
-
+	            initialize();
 	            //gets file name and displays it in title
 	            var name = $('#get-img')[0].files[0].name;
 	       	 	$('#img-title').attr('value', name);
@@ -53,11 +53,62 @@ $(document).ready(function(){
 
 	    }
 	}
+function initialize() {
+        var map = new google.maps.Map(document.getElementById('map'), {
+          center: {lat: -33.8688, lng: 151.2195},
+          zoom: 13
+        });
+
+        var input = document.getElementById('pac-input');
+
+        var autocomplete = new google.maps.places.Autocomplete(input);
+        autocomplete.bindTo('bounds', map);
+
+        map.controls[google.maps.ControlPosition.TOP_LEFT].push(input);
+
+        var infowindow = new google.maps.InfoWindow();
+        var marker = new google.maps.Marker({
+          map: map
+        });
+        marker.addListener('click', function() {
+          infowindow.open(map, marker);
+        });
+
+        autocomplete.addListener('place_changed', function() {
+          infowindow.close();
+          var place = autocomplete.getPlace();
+          if (!place.geometry) {
+            return;
+          }
+
+          if (place.geometry.viewport) {
+            map.fitBounds(place.geometry.viewport);
+          } else {
+            map.setCenter(place.geometry.location);
+            map.setZoom(17);
+          }
+
+          // Set the position of the marker using the place ID and location.
+          marker.setPlace({
+            placeId: place.place_id,
+            location: place.geometry.location
+          });
+          marker.setVisible(true);
+          //set hidden form input to equal the placeID
+          $('#placeID').attr('value', place.place_id);
+          window.alert($('#placeID').val());
+
+          infowindow.setContent('<div><strong>' + place.name + '</strong><br>' +
+              place.formatted_address);
+          infowindow.open(map, marker);
+        });
+}
 
 	$("#get-img").change(function(){
 	    readURL(this);
 
 	});
 });
+
 
 
