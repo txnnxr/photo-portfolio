6217544a758d281bb3fda45c6af19b582dec28b8
Tanner Wells <tannerjameswells@gmail.com>
2016-09-21 11:50:13 -0400

    updates title font / also working on using upload form to send image to s3

--- app_server/controllers/admin.js
+++ app_server/controllers/admin.js
@@ -1,15 +1,17 @@
 var request = require('request');
+var Minio = require('minio');
+var fs = require('fs');
 var apiOptions = {
-	server: "http://localhost:3000"
+  server: "http://localhost:3000"
 };
 if(process.env.NODE_ENV === 'production'){
-	apiOptions.server = "https://txnnxr.herokuapp.com";
+  apiOptions.server = "https://txnnxr.herokuapp.com";
 }
 
 module.exports.uploadPage = function(req, res){
-	res.render('upload', {
-		title: 'Upload'
-	});
+  res.render('upload', {
+    title: 'Upload'
+  });
 }
 
 //route = /api/upload to get here
@@ -25,6 +27,13 @@ module.exports.upload = function(req, res){
     location: req.body.location,
     tags: req.body.tags //should split in router
   };
+  var s3Client = new Minio.Client({
+    endPoint: 's3.amazonaws.com',
+    accessKey: 'AKIAJRSHERNPNXHGUR7A',
+    secretKey: '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'
+  });
+  file = req.file.imgUpload.path;
+  console.log(file)
   requestOptions = {
     url : apiOptions.server + path,
     method : "POST",
@@ -32,11 +41,15 @@ module.exports.upload = function(req, res){
   };
   if (!postdata.title) {
     res.redirect('/upload');
-  } else {
-    request(
+  } else {  
+    s3Client.fPutObject('txnnxr-bucket', req.body.title, file, 'application/octet-stream', function(e) {
+      if (e) {
+        return console.log(e)
+      }
+      request(
       requestOptions, //getting stuck processing the request for some reason
       function(err, response, body) { //doesn't make it to the callback function
-      	console.log("makes it to the request") 
+        console.log("makes it to the request") 
         if (response.statusCode === 201) {
           res.redirect('/upload'); //this should go to some kind of upload confirmation
         } else if (response.statusCode === 400 && body.name && body.name === "ValidationError" ) {
@@ -47,5 +60,7 @@ module.exports.upload = function(req, res){
         }
       }
     );
+  });
+
   }
 }
