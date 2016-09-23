c633fbcb125d5fa5b5b223d999867c7c95471e64
Tanner Wells <tannerjameswells@gmail.com>
2016-09-23 14:51:37 -0400

    add folder

---
+++ public/Drax-Project-Website/js/script.js
@@ -0,0 +1,9 @@
+//for making things appear on scroll
+// $(document).scroll(function() {
+//   var y = $(this).scrollTop();
+//   if (y > 800) {
+//     $('.bottomMenu').fadeIn();
+//   } else {
+//     $('.bottomMenu').fadeOut();
+//   }
+// });
